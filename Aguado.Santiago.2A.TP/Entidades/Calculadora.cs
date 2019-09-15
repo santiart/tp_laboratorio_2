fb41610f7bad0ae0bd390d1f6a16312151e94607
santiart <47418108+santiart@users.noreply.github.com>
2019-09-15 16:24:00 -0300

    Add files via upload

---
+++ Aguado.Santiago.2A.TP/Entidades/Calculadora.cs
@@ -0,0 +1,61 @@
+﻿namespace Entidades
+{
+    public class Calculadora
+    {
+        /// <summary>
+        /// realiza la operacion que se elija
+        /// </summary>
+        /// <param name="num1">primer valor ingresado</param>
+        /// <param name="num2">segundo valor ingresado</param>
+        /// <param name="operador">operacion a realizar</param>
+        /// <returns>devuelve el resultado de la operacion que se pidio</returns>
+        public double Operar(Numero num1, Numero num2, string operador)
+        {
+            double retorno;
+
+            switch (ValidarOperador(operador))
+            {
+                case "/":
+                    retorno = num1 / num2;
+                    break;
+
+                case "-":
+                    retorno = num1 - num2;
+                    break;
+
+                case "*":
+                    retorno = num1 * num2;
+                    break;
+
+                case "+":
+                    retorno = num1 + num2;
+                    break;
+
+                default:
+                    retorno = num1 + num2;
+                    break;
+            }
+
+            return retorno;
+        }
+
+        /// <summary>
+        /// valida el operador ingresado como parametro
+        /// </summary>
+        /// <param name="operador">operador ingresado</param>
+        /// <returns>devuelve el operador validado</returns>
+        private static string ValidarOperador(string operador)
+        {
+            string retorno = "+";
+
+            if (operador == "-")
+                retorno = "-";
+            else if (operador == "/")
+                retorno = "/";
+            else if (operador == "*")
+                retorno = "*";
+
+            return retorno;
+        }
+    }
+}
---
+++ Aguado.Santiago.2A.TP/MiCalculadora/FormCalculadora.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+
+namespace MiCalculadora
+{
+    public partial class FormCalculadora : Form
+    {
+        Calculadora calculadora = new Calculadora();
+        public FormCalculadora()
+        {
+            this.StartPosition = FormStartPosition.CenterScreen;
+            InitializeComponent();
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnConverirABinario_Click(object sender, EventArgs e)
+        {
+            Numero number = new Numero(this.txtResultado.Text);
+            this.txtResultado.Text = number.DecimalBinario(this.txtResultado.Text);
+        }
+
+        private void btnConvertirADecimal_Click(object sender, EventArgs e)
+        {
+            Numero number = new Numero(this.txtResultado.Text);
+            this.txtResultado.Text = number.BinarioDecimal(this.txtResultado.Text);
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            this.txtResultado.ResetText();
+            this.txtNumero1.Clear();
+            this.txtNumero2.Clear();
+            this.cmbOperador.ResetText();
+
+        }
+
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            if(this.txtNumero1.Text != " " && this.txtNumero2.Text != " " && this.cmbOperador.SelectedItem != null)
+            {
+                Numero num1 = new Numero(this.txtNumero1.Text);
+                Numero num2 = new Numero(this.txtNumero2.Text);
+
+                this.txtResultado.Text = this.calculadora.Operar(num1, num2, this.cmbOperador.SelectedItem.ToString()).ToString();
+            }
+        }
+
+
+    }
+}
